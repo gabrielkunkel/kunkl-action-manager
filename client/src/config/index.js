9f3e10b752cbd4d7933057a19305ada84a09d0c4
Gabriel Kunkel <henrysaltandson@gmail.com>
2019-11-20 15:31:26 -0600

    add uuid service

---
+++ client/src/config/index.js
@@ -0,0 +1,15 @@
+export default {
+    api: {
+        server: 'localhost:3001/api'
+    },
+    apiRoutes: {
+        addAction: '/actions/add-or-update',
+        updateAction: '/actions/add-or-update',
+        getAllActions: '/actions/get-all',
+        getOneAction: '/actions/get-one',
+        addUser: '/users',
+        updateUser: '/users',
+        getUser: '/users/user'
+    }
+
+}
---
+++ client/src/services/db.service.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export function RegisterNewUser(obj) {
+
+}
+
+export default {
+    RegisterNewUser
+}
