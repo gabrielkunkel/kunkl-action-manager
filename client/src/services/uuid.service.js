834e1b25c45d5db688ac5f763b0126ee65304a0b
Gabriel Kunkel <henrysaltandson@gmail.com>
2019-11-25 10:32:58 -0600

    add eslint to uuid.service

--- client/src/services/uuid.service.js
+++ client/src/services/uuid.service.js
@@ -6,12 +6,15 @@ function generateUUID() { // Public Domain/MIT
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = Math.random() * 16;
         if(d > 0){
+            // eslint-disable-next-line
             r = (d + r)%16 | 0;
             d = Math.floor(d/16);
         } else {
+            // eslint-disable-next-line
             r = (d2 + r)%16 | 0;
             d2 = Math.floor(d2/16);
         }
+        // eslint-disable-next-line
         return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
     });
 }
